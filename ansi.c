4e234069ae8203eb5beb1597b8cb1ea845dd65ce
dargaCode <darga.code@gmail.com>
2015-10-06 23:54:23 -0400

    experimenting with modular macros for formatting

--- ansi.c
+++ ansi.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
 // printf ANSI macros
-#define COLOR_RESET     "\x1b[0m"
-#define COLOR_BRIGHT    "\x1b[1m"
-#define COLOR_FAINT     "\x1b[2m"
-#define COLOR_FG_BLACK     "\x1b[30m"
-#define COLOR_FG_RED       "\x1b[31m"
-#define COLOR_FG_GREEN     "\x1b[32m"
-#define COLOR_FG_YELLOW    "\x1b[33m"
-#define COLOR_FG_BLUE      "\x1b[34m"
-#define COLOR_FG_MAGENTA   "\x1b[35m"
-#define COLOR_FG_CYAN      "\x1b[36m"
-#define COLOR_FG_WHITE     "\x1b[37m"
+#define ANSI   "\x1b["
+#define COLOR_RESET   "\x1b[0m"
+#define AND     ";"
+#define DONE    "m"
+#define BRIGHT  "1"
+#define FAINT   "2"
+#define PLAIN   "22"
+#define FG      "3"
+#define BG      "4"
+#define BLACK   "0"
+#define RED     "1"
+#define GREEN   "2"
+#define YELLOW  "3"
+#define BLUE    "4"
+#define MAGENTA "5"
+#define CYAN    "6"
+#define WHITE   "7"
+
+#define TEST1 ANSI FG YELLOW AND BG GREEN DONE
+#define TEST2 ANSI BG BLUE AND PLAIN AND FG GREEN AND BRIGHT DONE
 
 int main()
 {
-    printf(COLOR_FG_RED);
-    printf("asdf");
-    printf(COLOR_FG_GREEN);
-    printf("more");
-    printf(COLOR_BRIGHT);
-    printf("stuff\n");
+    printf(TEST1);
+    printf("ANSI MACRO TEST 1 ");
+    printf(TEST2);
+    printf("ANSI MACRO TEST 2");
     printf(COLOR_RESET);
+
+    printf("\n\n");
+
+    printf("\x1b[2;41;32m");
+    printf("ANSI MACRO TEST 3 ");
+    printf("\x1b[22;45;34m");
+    printf("ANSI MACRO TEST 4");
+    printf(COLOR_RESET);
+
+    printf("\n\n");
+
 }
