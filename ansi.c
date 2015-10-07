3a0981b51c6f60e189bbc7439c39911d217ed7a1
dargaCode <darga.code@gmail.com>
2015-10-06 23:05:16 -0400

    added macros for all foreground colors

--- ansi.c
+++ ansi.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
 // printf ANSI macros
+#define COLOR_RESET     "\x1b[0m"
 #define COLOR_BRIGHT    "\x1b[1m"
 #define COLOR_FAINT     "\x1b[2m"
-#define COLOR_RESET     "\x1b[0m"
-#define COLOR_BLACK
-#define COLOR_RED
-#define COLOR_GREEN
-#define COLOR_YELLOW
-#define COLOR_BLUE
-#define COLOR_MAGENTA
-#define COLOR_CYAN
-#define COLOR_WHITE
+#define COLOR_FG_BLACK     "\x1b[30m"
+#define COLOR_FG_RED       "\x1b[31m"
+#define COLOR_FG_GREEN     "\x1b[32m"
+#define COLOR_FG_YELLOW    "\x1b[33m"
+#define COLOR_FG_BLUE      "\x1b[34m"
+#define COLOR_FG_MAGENTA   "\x1b[35m"
+#define COLOR_FG_CYAN      "\x1b[36m"
+#define COLOR_FG_WHITE     "\x1b[37m"
 
 int main()
 {
-    printf("sup\n");
-    printf(COLOR_FAINT);
-    printf("sup\n");
+    printf(COLOR_FG_RED);
+    printf("asdf");
+    printf(COLOR_FG_GREEN);
+    printf("more");
     printf(COLOR_BRIGHT);
-    printf("sup\n");
+    printf("stuff\n");
     printf(COLOR_RESET);
 }
